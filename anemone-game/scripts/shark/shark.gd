624e2007b95ceeda660a3dd999829c2af8a69ceb
Leo Schurrer <l.schurrer@oth-aw.de>
2025-11-09 05:59:44 +0100

    SharkAI

--- anemone-game/scripts/shark/shark.gd
+++ anemone-game/scripts/shark/shark.gd
@@ -3,15 +3,31 @@ extends Node2D
 @export var acceleration: float
 @export var maxSpeed: float
 
-var _vel = Vector2.ZERO
+@export var next_position_timer: Timer
 
-func _process(delta):
-	var targetPos = get_viewport().get_mouse_position()
-	
-	if (targetPos != position):
-		_vel += (targetPos - position).limit_length(acceleration) * delta
-		_vel = _vel.limit_length(maxSpeed)
-		position = position + (_vel * delta)
-		
-		look_at(position + _vel)
-		rotation += PI / 2
+@export var fish: Fish
+
+var map_size: Vector2
+
+var target: Player
+var random_point: Vector2
+#var hunt: bool = true
+
+func _process(_delta):	
+	if not target.is_hidden:
+		if not next_position_timer.is_stopped():
+			next_position_timer.stop()
+		fish.target_position = target.global_position
+	elif next_position_timer.is_stopped():
+		_target_random_point_on_map()
+		next_position_timer.wait_time = 0
+		next_position_timer.start()
+
+
+func _target_random_point_on_map():
+	random_point = Vector2(randf_range(0.0, map_size.x), randf_range(0.0, map_size.y))
+	fish.target_position = random_point
+
+
+func _on_random_clock_timeout() -> void:
+	_target_random_point_on_map()
